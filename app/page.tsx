89e739132e3a98f2a0ea80e74c76257ebbc6d25d
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-08-28 05:18:08 +0200

    modification nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Navbar from "@/components/widgets/navbar";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-dvh flex flex-col items-start justify-start lg:flex-row">
       <Navbar />
-    </>
+      <section className="flex flex-col items-center justify-center w-full h-dvh">
+        <h1>Home</h1>
+      </section>
+    </div>
   );
 }
 
--- components/widgets/navbar.tsx
+++ components/widgets/navbar.tsx
@@ -14,39 +14,37 @@ export default function Navbar() {
   });
 
   return (
-    <aside>
-      <nav className="flex justify-start w-1/5">
-        <Sheet>
-          <SheetTrigger className="lg:hidden">
-            <RiMenu4Line size={40} />
-          </SheetTrigger>
-          <SheetContent className="h-dvh flex flex-col items-center justify-center gap-10">
-            <SheetHeader>
-              <Image
-                src="/assets/logo.png"
-                alt="Creanumerik04 logo"
-                width={50}
-                height={50}
-              />
-            </SheetHeader>
-            <ul className="flex flex-col items-center justify-center gap-10">
-              {links}
-            </ul>
-          </SheetContent>
-        </Sheet>
-        <ul className="hidden lg:flex flex-col lg:items-center lg:justify-center lg:gap-10 lg:mx-auto lg:h-dvh lg:text-xl">
-          <li>
+    <nav className="h-10 lg:h-dvh lg:w-1/5">
+      <Sheet>
+        <SheetTrigger className="lg:hidden">
+          <RiMenu4Line size={40} />
+        </SheetTrigger>
+        <SheetContent className="h-dvh flex flex-col items-center justify-center gap-10">
+          <SheetHeader>
             <Image
               src="/assets/logo.png"
               alt="Creanumerik04 logo"
               width={50}
               height={50}
             />
-          </li>
-          {links}
-        </ul>
-      </nav>
-    </aside>
+          </SheetHeader>
+          <ul className="flex flex-col items-center justify-center gap-10">
+            {links}
+          </ul>
+        </SheetContent>
+      </Sheet>
+      <aside className="hidden lg:flex lg:flex-col lg:items-center lg:justify-center lg:gap-10 lg:mx-auto lg:h-dvh lg:text-xl lg:list-none">
+        <li>
+          <Image
+            src="/assets/logo.png"
+            alt="Creanumerik04 logo"
+            width={50}
+            height={50}
+          />
+        </li>
+        {links}
+      </aside>
+    </nav>
   );
 }
 
