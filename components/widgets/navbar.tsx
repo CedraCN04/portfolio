745cc8fb3408cd238c5646ad01fd2be2e9961cd7
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-08-28 18:57:37 +0200

    Ajout de la page d'accueil

--- components/widgets/navbar.tsx
+++ components/widgets/navbar.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { RiMenu4Line } from "react-icons/ri";
 import { LINKS } from "../../lib/constants";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 
 export default function Navbar() {
   const links = LINKS.map((link) => {
@@ -14,35 +20,39 @@ export default function Navbar() {
   });
 
   return (
-    <nav className="h-10 lg:h-dvh lg:w-1/5">
+    <nav className="h-10 md:h-dvh md:w-1/5">
       <Sheet>
-        <SheetTrigger className="lg:hidden">
+        <SheetTrigger className="md:hidden">
           <RiMenu4Line size={40} />
         </SheetTrigger>
         <SheetContent className="h-dvh flex flex-col items-center justify-center gap-10">
           <SheetHeader>
+            <SheetTitle>
+              <Image
+                src="/assets/logo.png"
+                alt="Creanumerik04 logo"
+                width={50}
+                height={50}
+              />
+            </SheetTitle>
+          </SheetHeader>
+          <ul className="flex flex-col items-center justify-center gap-10 list-none">
+            {links}
+          </ul>
+        </SheetContent>
+      </Sheet>
+      <aside>
+        <ul className="hidden md:flex md:flex-col md:items-center md:justify-center md:gap-10 md:mx-auto md:h-dvh lg:text-xl md:list-none">
+          <li>
             <Image
               src="/assets/logo.png"
               alt="Creanumerik04 logo"
               width={50}
               height={50}
             />
-          </SheetHeader>
-          <ul className="flex flex-col items-center justify-center gap-10">
-            {links}
-          </ul>
-        </SheetContent>
-      </Sheet>
-      <aside className="hidden lg:flex lg:flex-col lg:items-center lg:justify-center lg:gap-10 lg:mx-auto lg:h-dvh lg:text-xl lg:list-none">
-        <li>
-          <Image
-            src="/assets/logo.png"
-            alt="Creanumerik04 logo"
-            width={50}
-            height={50}
-          />
-        </li>
-        {links}
+          </li>
+          {links}
+        </ul>
       </aside>
     </nav>
   );
--- types/type.d.ts
+++ types/type.d.ts
@@ -1,5 +1,15 @@
+import { IconType } from "react-icons/lib";
+
+// Liens barre de navigation
 export type NavLinks = {
   id: string;
   title: string;
   href: string;
 };
+
+// Liens vers les réseaux sociaux
+export type SocialLinks = {
+  id: string,
+  href: string,
+  icon: IconType,
+}
