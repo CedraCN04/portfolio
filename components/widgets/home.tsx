c657e24055ed9de7cca221f87312baf56ee2add1
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-09-28 22:31:57 +0200

    ajout texte page d'accueil

--- components/widgets/home.tsx
+++ components/widgets/home.tsx
@@ -7,16 +7,23 @@ export default function HomePage() {
         Guillaume Ceccoli
       </h1>
       <h2 className="text-base md:text-2xl">Développeur React</h2>
-      <p className="md:w-3/5 text-sm md:text-base">
-        Bonjour, bienvenue sur mon portfolio ! Passionné par le développement
-        web, le design et l&apos;impression 3D, j&apos;ai décidé de quitter mon
-        emploi de menuisier en 2022, afin de me reconvertir dans le domaine du
-        web. Orienté principalement Frontend, j&apos;ai également quelques
-        connaissances en Backend, que je consolide chaque jour, en réalisant des
-        projets personnels concrets. Curieux et dynamique, je suis toujours en
-        veille et me tient informé sur les dernières avancées dans le domaine du
-        web.
-      </p>
+      <div className="md:w-3/5 text-sm md:text-base">
+        <p className="mb-10">
+          Bonjour, bienvenue sur mon portfolio ! Passionné par le développement
+          web, le design et l&apos;impression 3D, j&apos;ai décidé de quitter
+          mon emploi de menuisier en 2022, afin de me reconvertir dans le
+          domaine du web. Orienté principalement Frontend, j&apos;ai également
+          quelques connaissances en Backend, que je consolide chaque jour, en
+          réalisant des projets personnels concrets. Curieux et dynamique, je
+          suis toujours en veille et me tient informé sur les dernières avancées
+          dans le domaine du web.
+        </p>
+        <span className="italic text-xs">
+          Ce portfolio est en constante évolution, n&apos;hésitez donc pas à
+          repasser pour voir les améliorations que j&apos;y apporte
+          régulièrement, ainsi que les nouveaux projets !
+        </span>
+      </div>
       <Social />
     </article>
   );
