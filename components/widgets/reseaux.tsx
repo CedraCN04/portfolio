04e535effe1231dfbf85547d73d4365b846ce10d
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-09-28 22:42:51 +0200

    ajout animation icon reseaux sociaux

--- components/widgets/reseaux.tsx
+++ components/widgets/reseaux.tsx
@@ -6,7 +6,10 @@ export default function Social() {
     <div className="flex flex-row items-center justify-between gap-4 w-3/5 md:w-1/5">
       {SOCIAL_LINKS.map((sociallink) => (
         <Link key={sociallink.id} href={sociallink.href}>
-          <sociallink.icon size={50} />
+          <sociallink.icon
+            size={50}
+            className="hover:scale-125 hover:text-gray-400 duration-500"
+          />
         </Link>
       ))}
     </div>
